65eaf2a8db6e9988d44de8f767f5174a0cfdfbf6
Hanna <hannarebeccadouglas.sia@gmail.com>
2024-03-09 11:23:46 -0500

    Buttons
    Made title and credit buttons functional, still needs background and other scenes

--- Assets/TitleScript.cs
+++ Assets/TitleScript.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour
 {
+    public Button StartButton;
+    public Button CreditButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void startbutton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 3);
+
+    }
+    public void creditbutton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 2);
     }
 }
